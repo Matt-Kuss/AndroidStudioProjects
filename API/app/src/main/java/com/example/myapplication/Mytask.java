a41f2b572ccea1353b71c4cfa0d4b516bad4a572
Matt-Kuss <matheus.kuss@ufpr.br>
2023-01-26 00:46:49 -0300

    1

--- API/app/src/main/java/com/example/myapplication/Mytask.java
+++ API/app/src/main/java/com/example/myapplication/Mytask.java
@@ -1,4 +1,37 @@
 package com.example.myapplication;
 
-public class Mytask {
+public class MyTask extends AsyncTask<String, Void, String> {
+
+    TextView output;
+
+
+    public Mytask(TextView output){
+        this.output = output;
+    }
+
+    @Override protected String doInBackground(String... strings) {
+        String stringURL = strings[0]) ;
+        InputStream inputStream = null;
+        InputStreamReader inputStreamReader = null;
+        StringBuffer buffer = null;
+
+        try {
+            URL url = new URL(stringURL);
+            HttpURLConnection connection = (HttpURLConnection) url;
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute(){
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(String s){
+        super.onPostExecute(s);
+    }
+
+
 }
+
